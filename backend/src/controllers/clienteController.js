56847cdcb2cb43d29b6275fd651980fa61a29642
joaquinyjoa <joaquinalfredogreco@gmail.comgit config --global user.email joaquinalfredogreco@gmail.com>
2026-03-07 17:54:34 -0300

    Creacion del controlador del cliente

---
+++ backend/src/controllers/clienteController.js
@@ -0,0 +1,28 @@
+const clienteService = require('../services/cliente.service');
+
+exports.getById = async (req, res, next) => {
+    try {
+        const cliente = await clienteService.getById(req.params.id);
+        res.status(200).json(cliente);
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.update = async (req, res, next) => {
+    try {
+        const cliente = await clienteService.update(req.params.id, req.body);
+        res.status(200).json(cliente);
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.delete = async (req, res, next) => {
+    try {
+        await clienteService.delete(req.params.id);
+        res.status(200).json({ message: 'Cliente eliminado correctamente' });
+    } catch (error) {
+        next(error);
+    }
+}
