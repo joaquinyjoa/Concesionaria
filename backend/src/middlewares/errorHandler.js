063c109963c320e1fe5dd4abd64444a32a9cbfea
joaquinyjoa <joaquinalfredogreco@gmail.comgit config --global user.email joaquinalfredogreco@gmail.com>
2026-03-08 16:14:18 -0300

    Manejo de errores para las imagenes

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -5,7 +5,9 @@ function errorHandler(err, req, res, next) {
         'Vehículo no encontrado',
         'Cliente no encontrado',
         'Empleado no encontrado',
-        'Usuario no encontrado'
+        'Usuario no encontrado',
+        'Venta no encontrada',
+        'Imagen no encontrada' 
     ];
     if (erroresNotFound.includes(err.message)) {
         return res.status(404).json({ error: err.message });
@@ -19,7 +21,11 @@ function errorHandler(err, req, res, next) {
         'no es válido',
         'debe tener',
         'mayor a 0',
-        'es demasiado corto'
+        'es demasiado corto',
+        'No se envió ninguna imagen',        
+        'Solo se permiten imágenes',         
+        'no puede tener más de 3 imágenes',  
+        'debe estar reservado'
     ];
     if (erroresValidacion.some(e => err.message.includes(e))) {
         return res.status(400).json({ error: err.message });
@@ -30,15 +36,17 @@ function errorHandler(err, req, res, next) {
         return res.status(400).json({ error: err.message });
     }
 
+     // 400 - Documento duplicado
+    if (err.code === '23505') {
+        return res.status(400).json({ error: 'El documento ya está registrado' });
+    }
+
     // 401 - Credenciales incorrectas
     if (err.message === 'Email o contraseña incorrectos') {
         return res.status(401).json({ error: err.message });
     }
     
-    if (err.code === '23505') {
-        return res.status(400).json({ error: 'El documento ya está registrado' });
-    }
-
+    // 403 - Sin permisos
     if (err.message.includes('No tenés permisos')) {
         return res.status(403).json({ error: err.message });
     }   
