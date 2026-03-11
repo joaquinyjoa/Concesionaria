30a36c101d7854d63f2748309b4564958b3e456b
joaquinyjoa <joaquinalfredogreco@gmail.comgit config --global user.email joaquinalfredogreco@gmail.com>
2026-03-11 10:16:20 -0300

    axios baseURL desde env

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: '/concesionaria'
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/concesionaria',
 })
 
 // interceptor que agrega el token en cada request automáticamente
