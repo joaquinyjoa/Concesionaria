dd7475d34fffc0415e4803f0c6d70c8a4bf958aa
joaquinyjoa <joaquinalfredogreco@gmail.comgit config --global user.email joaquinalfredogreco@gmail.com>
2026-03-16 13:26:46 -0300

    Cambio al subir una imagen

--- frontend/src/api/imagenes.js
+++ frontend/src/api/imagenes.js
@@ -1,22 +1,16 @@
-import axios from 'axios'
+import api from './axios'
 
 export const subirImagen = async (vehiculoId, formData) => {
-    const usuario = JSON.parse(localStorage.getItem('usuario'))
-    const res = await axios.post(
-        `http://localhost:3000/concesionaria/vehiculos/${vehiculoId}/imagenes`,
+    const res = await api.post(
+        `/vehiculos/${vehiculoId}/imagenes`,
         formData,
-        {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-                'Authorization': `Bearer ${usuario?.token}`
-            }
-        }
+        { headers: { 'Content-Type': 'multipart/form-data' } }
     )
     return res.data
 }
 
 export const getImagenesByVehiculo = (vehiculoId) =>
-    import('./axios').then(m => m.default.get(`/vehiculos/${vehiculoId}/imagenes`).then(r => r.data))
+    api.get(`/vehiculos/${vehiculoId}/imagenes`).then(r => r.data)
 
 export const eliminarImagen = (id) =>
-    import('./axios').then(m => m.default.delete(`/imagenes/${id}`).then(r => r.data))
+    api.delete(`/imagenes/${id}`).then(r => r.data)
